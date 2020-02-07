f954be5d3a8b4428b8768bf60e64eedd089f8ba7
Mat <msochacki5@gmail.com>
2020-02-07 12:52:07 +0100

    managing dictionaries and translations, improved criteria api fetch

--- src/components/EditDictionariesTable.js
+++ src/components/EditDictionariesTable.js
@@ -18,13 +18,40 @@ export default function EditDictionariesTable(props) {
         axios.post("http://localhost:8080/addDictionary", {
             name: newData.name,
             language: newData.language,
-            userId: 2
+            userId: 2 // <--------------------------------------------------------
         }).then(function (response) {
             setState(prevState => {
                 const data = [...prevState.data];
                 data.push(response.data);
                 return {...prevState, data};
             });
+        }).catch(function (error) {
+            console.log(error);
+        });
+    };
+
+    const deleteDictionary = oldData => {
+
+        axios.post("http://localhost:8080/deleteDictionaryById", {
+            id: oldData.id
+        }).then(function (response) {
+            props.setTranslationsLoaded(false);
+            console.log(response.data);
+        }).catch(function (error) {
+            console.log(error);
+        });
+    };
+
+    const modifyDictionary = newData => {
+
+        axios.put("http://localhost:8080/modifyDictionary", {
+            id: newData.id,
+            name: newData.name,
+            language: newData.language,
+            userId: newData.userId
+        }).then(function (response) {
+            props.setTranslationsLoaded(false);
+            console.log(response.data, 'modify pierwszy then')
         }).catch(function (error) {
             console.log(error);
         });
@@ -67,6 +94,7 @@ export default function EditDictionariesTable(props) {
                         setTimeout(() => {
                             resolve();
                             if (oldData) {
+                                modifyDictionary(newData);
                                 setState(prevState => {
                                     const data = [...prevState.data];
                                     data[data.indexOf(oldData)] = newData;
@@ -79,6 +107,7 @@ export default function EditDictionariesTable(props) {
                     new Promise(resolve => {
                         setTimeout(() => {
                             resolve();
+                            deleteDictionary(oldData);
                             setState(prevState => {
                                 const data = [...prevState.data];
                                 data.splice(data.indexOf(oldData), 1);
