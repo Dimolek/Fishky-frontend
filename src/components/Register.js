15b2811a551e7fb0965e5fed894650ff7e24af2c
Dimol <dimolek@gmail.com>
2020-02-11 15:03:32 +0100

    Fixing unit tests, user validator and policy

--- src/components/Register.js
+++ src/components/Register.js
@@ -12,7 +12,6 @@ import Container from '@material-ui/core/Container';
 import {Link} from "react-router-dom";
 import Paper from "@material-ui/core/Paper";
 
-
 const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1,
@@ -36,11 +35,11 @@ const useStyles = makeStyles(theme => ({
         flexDirection: 'column',
         alignItems: 'center',
         backgroundColor: "#FFFFFF",
-        borderRadius:'22px',
-        paddingRight:'25px',
-        paddingLeft:'25px',
-        paddingBottom:'15px',
-        paddingTop:'5px'
+        borderRadius: '22px',
+        paddingRight: '25px',
+        paddingLeft: '25px',
+        paddingBottom: '15px',
+        paddingTop: '5px'
     },
     avatar: {
         margin: theme.spacing(1),
@@ -78,6 +77,18 @@ const CssTextField = withStyles({
     },
 })(TextField);
 
+const handleSubmit = event => {
+    const axios = require('axios').default;
+
+    axios.post("http://localhost:8080/addUser", {
+        //todo
+    }).then(function (response) {
+        console.log('sukces');
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
 function Register() {
     const classes = useStyles();
 
@@ -86,27 +97,27 @@ function Register() {
             <Container component="main" maxWidth="xs" style={{'paddingTop': 70}}>
                 <CssBaseline/>
                 <Paper elevation={12} className={classes.paper}>
-                        <Avatar className={classes.avatar}>
-                            <LockOutlinedIcon/>
-                        </Avatar>
-                        <Typography component="h1" variant="h5">
-                            Create account
-                        </Typography>
-                        <form className={classes.form}>
-                            <Grid container spacing={2}>
-                                <Grid item xs={12}>
-                                    <CssTextField
-                                        className={classes.margin}
-                                        autoComplete="Username"
-                                        name="username"
-                                        variant="outlined"
-                                        required
-                                        fullWidth
-                                        id="username"
-                                        label="Username"
-                                        autoFocus
-                                    />
-                                   {/* <TextField
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon/>
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Create account
+                    </Typography>
+                    <form className={classes.form}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <CssTextField
+                                    className={classes.margin}
+                                    autoComplete="Username"
+                                    name="username"
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="username"
+                                    label="Username"
+                                    autoFocus
+                                />
+                                {/* <TextField
                                         autoComplete="Username"
                                         name="username"
                                         variant="outlined"
@@ -118,19 +129,19 @@ function Register() {
                                         //value={this.state.userName}
                                         //onChange={this.handleUserName}
                                     />*/}
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <CssTextField
-                                        className={classes.margin}
-                                        variant="outlined"
-                                        required
-                                        fullWidth
-                                        name="password"
-                                        label="Password"
-                                        type="password"
-                                        id="password"
-                                    />
-                                    {/*<TextField
+                            </Grid>
+                            <Grid item xs={12}>
+                                <CssTextField
+                                    className={classes.margin}
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                />
+                                {/*<TextField
                                         variant="outlined"
                                         required
                                         fullWidth
@@ -141,26 +152,38 @@ function Register() {
                                         //value={this.state.password}
                                         //onChange={this.handlePassword}
                                     />*/}
-                                </Grid>
                             </Grid>
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                color="primary"
-                                className={classes.submit}
-                                //onClick={this.createUser}
-                            >
-                                Register
-                            </Button>
-                            <Grid container justify="flex-end">
-                                <Grid item>
-                                    <Link to="/Login" variant="body2">
-                                        Already have an account? Sign in!
-                                    </Link>
-                                </Grid>
+                            <Grid item xs={12}>
+                                <CssTextField
+                                    className={classes.margin}
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    name="confirmpassword"
+                                    label="Confirm Password"
+                                    type="confirmpassword"
+                                    id="confirmpassword"
+                                />
                             </Grid>
-                        </form>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            onClick={handleSubmit}
+                        >
+                            Register
+                        </Button>
+                        <Grid container justify="flex-end">
+                            <Grid item>
+                                <Link to="/Login" variant="body2">
+                                    Already have an account? Sign in!
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </form>
 
                 </Paper>
             </Container>
