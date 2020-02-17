309247fb2e81690fb6723e7bd6b1044b1d78cc92
Mat <msochacki5@gmail.com>
2020-02-17 19:46:19 +0100

    logo

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {makeStyles} from "@material-ui/core";
 import Typography from "@material-ui/core/Typography/Typography";
-import placeholder from "../../images/placeholder.jpg";
+import logo from "../../images/logo.png";
 
 
 const useStyles = makeStyles(theme => ({
@@ -30,10 +30,7 @@ function App() {
     return (
         <div className={classes.background}>
             <div className={classes.content}>
-                <Typography variant="h1">
-                    Fishky
-                </Typography>
-                <img src={placeholder} alt="placeholder" />
+                <img src={logo} alt="logo" />
                 <Typography variant='h3'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus leo non est. Etiam sit amet lectus quis est congue mollis. Phasellus congue lacus eget neque. Phasellus ornare, ante vitae consectetuer consequat, purus sapien ultricies dolor, et mollis pede metus eget nisi. Praesent sodales velit quis augue. Cras suscipit, urna at aliquam rhoncus, urna quam viverra nisi, in interdum massa nibh nec erat.
                 </Typography>
