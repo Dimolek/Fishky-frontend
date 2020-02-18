d9c8779b44a1074f300971230af1d16a23f53494
Mat <msochacki5@gmail.com>
2020-02-18 11:36:01 +0100

    homepage spacing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -16,12 +16,15 @@ const useStyles = makeStyles(theme => ({
         backgroundColor:"#0D870B",
     },
     content: {
-        fontFamily:"Arial",
         textAlign: 'center',
-        fontSize: 40,
-        padding: theme.spacing(3),
-        fontWeight: 600,
     },
+    image: {
+        padding: theme.spacing(4)
+    },
+    text: {
+        paddingLeft: theme.spacing(20),
+        paddingRight: theme.spacing(20)
+    }
 }));
 
 function App() {
@@ -30,9 +33,12 @@ function App() {
     return (
         <div className={classes.background}>
             <div className={classes.content}>
-                <img src={logo} alt="logo" />
-                <Typography variant='h3'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus leo non est. Etiam sit amet lectus quis est congue mollis. Phasellus congue lacus eget neque. Phasellus ornare, ante vitae consectetuer consequat, purus sapien ultricies dolor, et mollis pede metus eget nisi. Praesent sodales velit quis augue. Cras suscipit, urna at aliquam rhoncus, urna quam viverra nisi, in interdum massa nibh nec erat.
+                <div className={classes.image}>
+                    <img src={logo} alt="logo"/>
+                </div>
+                <Typography variant='h4' className={classes.text}>
+                    An Internet application to help you train your language skills by imitating flashcards (Polish: fiszki).
+                    Create an account to start creating your own dictionaries and start practising!
                 </Typography>
             </div>
         </div>
